eba499a30bebca33039353c57ffbb77d912b3873
Max_xie <Max_xie@192.168.0.85>
2022-10-29 20:08:01 -0300

    Modificada la clase sistema

--- src/ar/edu/unq/po2/TPFinal/Sistema.java
+++ src/ar/edu/unq/po2/TPFinal/Sistema.java
@@ -1,41 +1,40 @@
 package ar.edu.unq.po2.TPFinal;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
+
 
 
 
 public class Sistema {
-	private Collection<Desafio>  desafios  = new ArrayList<Desafio>();
-	private Collection<Proyecto> proyectos = new ArrayList<Proyecto>();
-	private Collection<Usuario>  usuarios  = new ArrayList<Usuario>();
+	private List<Desafio>  desafios;
+	private List<Proyecto> proyectos; 
+	private List<Usuario>  usuarios;  
 	
 		
-	public Sistema(Collection<Desafio> desafios, Collection<Proyecto> proyectos, Collection<Usuario> usuarios) {
-		this.desafios = desafios;
-		this.proyectos = proyectos;
-		this.usuarios = usuarios;
+	public Sistema(List<Desafio> desafios, List<Proyecto> proyectos, List<Usuario> usuarios) {
+		this.desafios  = new ArrayList<Desafio>(); 
+		this.proyectos = new ArrayList<Proyecto>();
+		this.usuarios  = new ArrayList<Usuario>();
 	}
-	public Collection<Desafio> getDesafios() {
+	public List<Desafio> getDesafios() {
 		return desafios;
 	}
-	public void setDesafios(Collection<Desafio> desafios) {
+	public void setDesafios(List<Desafio> desafios) {
 		this.desafios = desafios;
 	}
-	public Collection<Proyecto> getProyectos() {
+	public List<Proyecto> getProyectos() {
 		return proyectos;
 	}
-	public void setProyectos(Collection<Proyecto> proyectos) {
+	public void setProyectos(List<Proyecto> proyectos) {
 		this.proyectos = proyectos;
 	}
-	public Collection<Usuario> getUsuarios() {
+	public List<Usuario> getUsuarios() {
 		return usuarios;
 	}
-	public void setUsuarios(Collection<Usuario> usuarios) {
+	public void setUsuarios(List<Usuario> usuarios) {
 		this.usuarios = usuarios;
 	}
-	
-	
 	
 	
 	
