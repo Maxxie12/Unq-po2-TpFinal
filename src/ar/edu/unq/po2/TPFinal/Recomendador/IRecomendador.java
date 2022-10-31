91660a8fc5fc4ab3ca5aaf680d0945b46a1ef3a6
MatiasGiraudo <maty.giraudo@hotmail.com>
2022-10-30 22:51:53 -0300

    Subo cambios recomendador

--- src/ar/edu/unq/po2/TPFinal/Recomendador/IRecomendador.java
+++ src/ar/edu/unq/po2/TPFinal/Recomendador/IRecomendador.java
@@ -2,9 +2,10 @@ package ar.edu.unq.po2.TPFinal.Recomendador;
 
 import java.util.List;
 
+import ar.edu.unq.po2.TPFinal.Desafio;
 import ar.edu.unq.po2.TPFinal.DesafioUsuario;
 import ar.edu.unq.po2.TPFinal.Usuario;
 
 public interface IRecomendador {
-	public List<DesafioUsuario> recomendarDesafios(Usuario usuario);//Recibiria tambien la lista de Desafio además del Usuario o las PreferenciasUsuario
+	public List<DesafioUsuario> recomendarDesafios(Usuario usuario, List<Desafio> desafios);
 }
--- src/ar/edu/unq/po2/TPFinal/Recomendador/RecomendadorPorCoincidencia.java
+++ src/ar/edu/unq/po2/TPFinal/Recomendador/RecomendadorPorCoincidencia.java
@@ -2,13 +2,14 @@ package ar.edu.unq.po2.TPFinal.Recomendador;
 
 import java.util.List;
 
+import ar.edu.unq.po2.TPFinal.Desafio;
 import ar.edu.unq.po2.TPFinal.DesafioUsuario;
 import ar.edu.unq.po2.TPFinal.Usuario;
 
 public class RecomendadorPorCoincidencia implements IRecomendador {
 
 	@Override
-	public List<DesafioUsuario> recomendarDesafios(Usuario usuario) {
+	public List<DesafioUsuario> recomendarDesafios(Usuario usuario, List<Desafio> desafios) {
 		// TODO Auto-generated method stub
 		return null;
 	}
