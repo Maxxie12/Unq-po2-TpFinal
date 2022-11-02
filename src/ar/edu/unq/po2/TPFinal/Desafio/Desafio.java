302884a872fb7a3cf07a0a199ffc720beaa3ffd9
MatiasGiraudo <maty.giraudo@hotmail.com>
2022-11-02 15:13:45 -0300

    Cambio tipo de restriccion en el Desafio

--- src/ar/edu/unq/po2/TPFinal/Desafio/Desafio.java
+++ src/ar/edu/unq/po2/TPFinal/Desafio/Desafio.java
@@ -1,8 +1,10 @@
+
 package ar.edu.unq.po2.TPFinal.Desafio;
 
 import ar.edu.unq.po2.TPFinal.Area;
 import ar.edu.unq.po2.TPFinal.Common.Dificultad;
 import ar.edu.unq.po2.TPFinal.Restriccion.IRestriccionTemporal;
+
 
 public class Desafio {
 	private 	Area area;
@@ -25,49 +27,6 @@ public class Desafio {
 	}
 
 	public IRestriccionTemporal getRestriccionTemporal() {
-		return restriccionTemporal;
-	}
-
-	public int getCantidadMuestrasARecolectar() {
-		return cantidadMuestrasARecolectar;
-	}
-
-	public Dificultad getDificultad() {
-		return dificultad;
-	}
-
-	public int getRecompensa() {
-		return recompensa;
-	}
-
-}
-package ar.edu.unq.po2.TPFinal.Desafio;
-
-import ar.edu.unq.po2.TPFinal.Area;
-import ar.edu.unq.po2.TPFinal.Common.Dificultad;
-import ar.edu.unq.po2.TPFinal.Restriccion.RestriccionTemporal;
-
-public class Desafio {
-	private 	Area area;
-	private 	RestriccionTemporal restriccionTemporal;
-	private int cantidadMuestrasARecolectar;
-	private Dificultad dificultad;
-	private int recompensa;
-	
-	public Desafio(Area area, RestriccionTemporal restriccionTemporal, int cantidadMuestrasARecolectar, Dificultad dificultad, int recompensa) {
-		this.area = area;
-		this.restriccionTemporal = restriccionTemporal;
-		this.cantidadMuestrasARecolectar = cantidadMuestrasARecolectar;
-		this.dificultad = dificultad;
-		this.recompensa = recompensa;
-		
-	}
-
-	public Area getArea() {
-		return area;
-	}
-
-	public RestriccionTemporal getRestriccionTemporal() {
 		return restriccionTemporal;
 	}
 
