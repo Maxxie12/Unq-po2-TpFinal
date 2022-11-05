b0b03cc1f9a4ea13fb9e0e334500132d9ae37800
Max_xie <Max_xie@192.168.0.85>
2022-11-05 19:31:07 -0300

    test buscador por titulo a medio hacer

--- test/ar/edu/unq/po2/TPFinal/BuscadorDeProyectos/BuscadorAndTest.java
+++ test/ar/edu/unq/po2/TPFinal/BuscadorDeProyectos/BuscadorAndTest.java
@@ -5,6 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class BuscadorAndTest {
+	
+	private BuscadorProyectos primerBuscador;
+	private BuscadorProyectos segundoBuscador;
+	
+	
 
 	@Test
 	void test() {
--- test/ar/edu/unq/po2/TPFinal/BuscadorDeProyectos/BuscadorPorTituloTest.java
+++ test/ar/edu/unq/po2/TPFinal/BuscadorDeProyectos/BuscadorPorTituloTest.java
@@ -1,14 +1,62 @@
 package ar.edu.unq.po2.TPFinal.BuscadorDeProyectos;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import ar.edu.unq.po2.TPFinal.Proyecto;
 
 class BuscadorPorTituloTest {
+	
+	private String fraseABuscar = "";
+	
+	
+	@Mock
+	Proyecto proyecto1;
+	@Mock
+    Proyecto proyecto2;
+	@Mock
+    Proyecto proyecto3;
+	@Mock
+    Proyecto proyecto4;
+	@Mock
+	BuscadorPorTitulo buscadorPorTitulo;   
+    
+	@BeforeEach
+    void setUp() {
+      List<Proyecto> proyectosAFiltrar = new ArrayList<Proyecto>();
+      
+      when(proyecto1.getNombre()).thenReturn("Alrededor del mundo.");
+      when(proyecto2.getNombre()).thenReturn("Evolución y procesos geológicos y geoambientales en la región pampeana.");
+      when(proyecto3.getNombre()).thenReturn("Separando las Aguas.");
+      when(proyecto4.getNombre()).thenReturn("Creando un tornado.");
+        
+        proyectosAFiltrar.add(proyecto1);
+		proyectosAFiltrar.add(proyecto2);     
+		proyectosAFiltrar.add(proyecto3);
+		proyectosAFiltrar.add(proyecto4);
+		System.out.println(proyecto1.getNombre());	
+    }
+    
+    
+   
+    @Test
+    void buscadorPorTitulo(List<Proyecto>proyectosAFiltrar){
+    	this.fraseABuscar = "procesos geológicos";
+    	
+    	assertTrue(proyectosAFiltrar.stream().filter(proyecto->proyecto.getNombre().contains(this.fraseABuscar)).toList().contains(proyecto2));
+    }
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
 
-}
+	 
+	 
+
+	
+	
+ }
