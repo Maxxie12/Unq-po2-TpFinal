30a1b82ec71d233b9823211579ce00fb9763ffe3
Max_xie <Max_xie@192.168.0.85>
2022-11-06 11:17:48 -0300

    implementado el test BuscadorPorCategorias

--- test/ar/edu/unq/po2/TPFinal/BuscadorDeProyectos/BuscadorPorCategoriasTest.java
+++ test/ar/edu/unq/po2/TPFinal/BuscadorDeProyectos/BuscadorPorCategoriasTest.java
@@ -16,7 +16,7 @@ import ar.edu.unq.po2.TPFinal.Common.Categoria;
 
 class BuscadorPorCategoriasTest {
     
-	private List<Categoria> categorias;
+	private List<Categoria> categorias = new ArrayList<Categoria>();
 
 	@Mock
 	BuscadorPorCategorias buscadorPorCategorias;
@@ -26,23 +26,23 @@ class BuscadorPorCategoriasTest {
     Categoria categoria4 = mock(Categoria.class);
     
 	@BeforeEach
-	void setUp(){	
-	when(categoria1.getDescripcion()).thenReturn("Geografía");
-    when(categoria2.getDescripcion()).thenReturn("Geología");
-    when(categoria3.getDescripcion()).thenReturn("Química");
-    when(categoria4.getDescripcion()).thenReturn("Química");
-    categorias.add(categoria1);
-    categorias.add(categoria2);
-    categorias.add(categoria3);
-    categorias.add(categoria4);
-	
+	void setUp() {
+		when(categoria1.getDescripcion()).thenReturn("Geografía");
+	    when(categoria2.getDescripcion()).thenReturn("Geología");
+	    when(categoria3.getDescripcion()).thenReturn("Química");
+	    when(categoria4.getDescripcion()).thenReturn("Química");
+	    categorias.add(categoria1);
+	    categorias.add(categoria2);
+	    categorias.add(categoria3);
+	    categorias.add(categoria4);
+	    
 	}
     
-	@Test
-	assertTrue(buscadorPorCategoria.categorias.contains(categoria2));
-
     
-    
+    @Test
+    void testBuscadorPorCategorias() {
+ assertTrue(buscadorPorCategorias.getCategorias().contains(categoria2));
+    }
     
 
 }
