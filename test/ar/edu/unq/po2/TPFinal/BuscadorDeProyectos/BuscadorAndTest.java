fcd7ed72c86f5b297fe8d0bd67c0214f6dea0ce8
Max_xie <Max_xie@192.168.0.85>
2022-11-05 21:00:25 -0300

    Merge branch 'main' of https://github.com/Maxxie12/Unq-po2-TpFinal.git into main

--- test/ar/edu/unq/po2/TPFinal/BuscadorDeProyectos/BuscadorAndTest.java
+++ test/ar/edu/unq/po2/TPFinal/BuscadorDeProyectos/BuscadorAndTest.java
@@ -2,7 +2,14 @@ package ar.edu.unq.po2.TPFinal.BuscadorDeProyectos;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import ar.edu.unq.po2.TPFinal.Proyecto;
 
 class BuscadorAndTest {
 	
@@ -10,10 +17,33 @@ class BuscadorAndTest {
 	private BuscadorProyectos segundoBuscador;
 	
 	
-
+	@Mock
+	Proyecto proyecto1;
+	@Mock
+    Proyecto proyecto2;
+	@Mock
+    Proyecto proyecto3;
+	@Mock
+    Proyecto proyecto4;
+	@Mock
+	BuscadorAnd buscadorAnd;
+	
+	@BeforeEach
+	void setUp() {
+	List<Proyecto> proyectosAFiltrar = new ArrayList<Proyecto>();
+	proyectosAFiltrar.add(proyecto1);
+	proyectosAFiltrar.add(proyecto2);     
+	proyectosAFiltrar.add(proyecto3);
+	proyectosAFiltrar.add(proyecto4);
+	
+ }
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void buscadorAnd(List<Proyecto>proyectosAFiltrar) {
+		
 	}
-
+	
+	
+	
+	
 }
