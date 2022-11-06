b3dea931e01c2dfe048341696ae7246d4550224b
MatiasGiraudo <maty.giraudo@hotmail.com>
2022-11-06 08:20:45 -0300

    Modifico test de busqueda por titulo

--- test/ar/edu/unq/po2/TPFinal/BuscadorDeProyectos/BuscadorPorTituloTest.java
+++ test/ar/edu/unq/po2/TPFinal/BuscadorDeProyectos/BuscadorPorTituloTest.java
@@ -1,8 +1,8 @@
 package ar.edu.unq.po2.TPFinal.BuscadorDeProyectos;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,45 +15,51 @@ import ar.edu.unq.po2.TPFinal.Proyecto;
 class BuscadorPorTituloTest {
 	
 	private String fraseABuscar = "";
+	List<Proyecto> proyectosAFiltrar = new ArrayList<Proyecto>();
 	
-	
 	@Mock
-	Proyecto proyecto1;
+	Proyecto proyecto1 = mock(Proyecto.class);
 	@Mock
-    Proyecto proyecto2;
+    Proyecto proyecto2= mock(Proyecto.class);;
 	@Mock
-    Proyecto proyecto3;
+    Proyecto proyecto3 = mock(Proyecto.class);;
 	@Mock
-    Proyecto proyecto4;
+    Proyecto proyecto4 = mock(Proyecto.class);;
 	@Mock
 	BuscadorPorTitulo buscadorPorTitulo;   
     
 	@BeforeEach
     void setUp() {
-      List<Proyecto> proyectosAFiltrar = new ArrayList<Proyecto>();
+      
       
       when(proyecto1.getNombre()).thenReturn("Alrededor del mundo.");
+
       when(proyecto2.getNombre()).thenReturn("Evolución y procesos geológicos y geoambientales en la región pampeana.");
       when(proyecto3.getNombre()).thenReturn("Separando las Aguas.");
       when(proyecto4.getNombre()).thenReturn("Creando un tornado.");
-        
+
         proyectosAFiltrar.add(proyecto1);
 		proyectosAFiltrar.add(proyecto2);     
 		proyectosAFiltrar.add(proyecto3);
 		proyectosAFiltrar.add(proyecto4);
-		System.out.println(proyecto1.getNombre());	
+	System.out.println(proyecto1.getNombre());
     }
     
     
    
     @Test
-    void buscadorPorTitulo(List<Proyecto>proyectosAFiltrar){
+    void testBuscadorPorTituloEncontrado(){
     	this.fraseABuscar = "procesos geológicos";
     	
     	assertTrue(proyectosAFiltrar.stream().filter(proyecto->proyecto.getNombre().contains(this.fraseABuscar)).toList().contains(proyecto2));
     }
 
-
+    @Test
+    void testBuscadorPorTituloNoEncontrado(){
+    	this.fraseABuscar = "titulo  inexistente";
+    	
+    	assertFalse(proyectosAFiltrar.stream().filter(proyecto->proyecto.getNombre().contains(this.fraseABuscar)).toList().contains(proyecto2));
+    }
 	 
 	 
 
