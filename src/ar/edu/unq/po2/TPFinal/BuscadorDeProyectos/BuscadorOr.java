c2a8d769c797acb2c7257a2f2350439ba4907bc9
MatiasGiraudo <maty.giraudo@hotmail.com>
2022-11-06 15:37:19 -0300

    Modifico filtro de buscador con Or para que retorne lo que devuelve una lista u otra, sin duplicados

--- src/ar/edu/unq/po2/TPFinal/BuscadorDeProyectos/BuscadorOr.java
+++ src/ar/edu/unq/po2/TPFinal/BuscadorDeProyectos/BuscadorOr.java
@@ -26,15 +26,12 @@ public class BuscadorOr extends BuscadorProyectos {
 		return this.segundoBuscador;
 	}
  public List<Proyecto> filtrar(List<Proyecto> proyectosAFiltrar){
-    List<Proyecto> proyectosAFiltrarConBuscador1 = new ArrayList <Proyecto>();
-	List<Proyecto> proyectosAFiltrarConBuscador2 = new ArrayList <Proyecto>();
+    List<Proyecto> proyectosResultantes = new ArrayList <Proyecto>();
 	
-	proyectosAFiltrarConBuscador1.addAll(this.getPrimerBuscador().filtrar(proyectosAFiltrar));
-	proyectosAFiltrarConBuscador2.addAll(this.getSegundoBuscador().filtrar(proyectosAFiltrar));
+    proyectosResultantes.addAll(this.getPrimerBuscador().filtrar(proyectosAFiltrar));
+    proyectosResultantes.addAll(this.getSegundoBuscador().filtrar(proyectosAFiltrar));
 	
-	proyectosAFiltrarConBuscador1.removeAll(proyectosAFiltrarConBuscador2); // Me quedo con los proyectos que NO se repiten en ambas listas
-	
-	return (proyectosAFiltrarConBuscador1);
+	return proyectosResultantes.stream().distinct().toList();
 	
  
  }
