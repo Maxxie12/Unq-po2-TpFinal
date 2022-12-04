191c9f8add91b9478eb5ca0e20d46efb047e2c38
MatiasGiraudo <maty.giraudo@hotmail.com>
2022-12-04 12:04:36 -0300

    Modifico buscador por categorias

--- src/ar/edu/unq/po2/TPFinal/BuscadorDeProyectos/BuscadorPorCategorias.java
+++ src/ar/edu/unq/po2/TPFinal/BuscadorDeProyectos/BuscadorPorCategorias.java
@@ -10,7 +10,7 @@ import ar.edu.unq.po2.TPFinal.Common.Categoria;
 
 public class BuscadorPorCategorias implements IBuscadorProyectos {
 	
-	private List<Categoria> categorias = new ArrayList<Categoria>();
+private List<Categoria> categorias = new ArrayList<Categoria>();
 	
 	public BuscadorPorCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
@@ -20,7 +20,7 @@ public class BuscadorPorCategorias implements IBuscadorProyectos {
 
 	@Override
 	public List<Proyecto> filtrar(List<Proyecto> proyectosAFiltrar) {
-		// TODO Auto-generated method stub
-		return null;
+		return proyectosAFiltrar.stream().filter(p -> p.getCategorias().containsAll(this.categorias)).toList();
+		
 	}
 }
