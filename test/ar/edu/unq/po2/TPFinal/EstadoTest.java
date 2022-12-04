917d11fead7cb1fb63205f965289bbc6af44dad1
Nicolas Carrizo <nikolascarrizo1997@gmail.com>
2022-12-04 12:30:05 -0300

    Arreglo tests de EstadoTest

--- test/ar/edu/unq/po2/TPFinal/EstadoTest.java
+++ test/ar/edu/unq/po2/TPFinal/EstadoTest.java
@@ -13,13 +13,11 @@ import ar.edu.unq.po2.TPFinal.Common.Dificultad;
 import ar.edu.unq.po2.TPFinal.Desafio.Desafio;
 import ar.edu.unq.po2.TPFinal.Desafio.DesafioUsuario;
 import ar.edu.unq.po2.TPFinal.Estado.EstadoCompletado;
-import ar.edu.unq.po2.TPFinal.Estado.EstadoVencido;
 import ar.edu.unq.po2.TPFinal.Restriccion.RestriccionFecha;
 
 public class EstadoTest {
 	
 	EstadoCompletado estadoCompletado;
-	EstadoVencido estadoVencido;
 	DesafioUsuario desafioUsuario;
 	
 	Desafio desafio;
@@ -35,19 +33,14 @@ public class EstadoTest {
 	public void setUp() {
 		desafio = new Desafio(area, restriccionFecha, 0, Dificultad.INTERMEDIO, 0);
 		estadoCompletado = new EstadoCompletado();
-		estadoVencido = new EstadoVencido();
 		desafioUsuario = new DesafioUsuario(desafio);
 	
 	}
 	
 	@Test
-	public void testDesafoCompletadop() {
-		estadoCompletado.desafioCompletado(desafioUsuario);
-		assertEquals(estadoCompletado.esDesafioCompletado(desafioUsuario), new EstadoCompletado().esDesafioCompletado(desafioUsuario));
+	public void testDesafioCompletado() {
+		assertEquals(estadoCompletado.getPorcentajeCompletititud(desafioUsuario), new EstadoCompletado().getPorcentajeCompletititud(desafioUsuario));
 	}
 
-	@Test
-	public void testDesafioVencido() {
-		assertFalse(estadoVencido.esDesafioCompletado(desafioUsuario));
-	}
+
 }
